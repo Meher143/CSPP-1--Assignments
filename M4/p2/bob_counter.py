1f765a681687b09b9181f757e1a2ac2ea4075903
Meher143 <krishmeher@msitprogram.net>
2018-08-02 19:56:34 +0530

    " -> 0 of 5 passed. pylint: -21.00/10 "

--- M4/p2/bob_counter.py
+++ M4/p2/bob_counter.py
@@ -1,11 +1,12 @@
 """string"""
 def main():
-    str_ = input()
-    str1_ = "bob"
-    coun_= 0
-    for i in range(len(str_-2)):
-          if str1_ == str_[i]+ str_[i+1]+str_[i+2]:
-                coun_ = coun_+1
-    print(coun_)
+   	"""string"""
+   	str0_ = input("")
+   	str1_ = 'bob'
+   	coun_ = 0
+   	for i in range(len(str_-2)):
+   		if str1_ == str_[i]+ str_[i+1]+str_[i+2]:
+   			coun_ = coun_+1
+   	print(coun_)
 if __name__== "__main__":
     main()
