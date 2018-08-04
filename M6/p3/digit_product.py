9b035fe9bcb2423ec7e53c9e3ad68aa59930dac9
Meher143 <krishmeher@msitprogram.net>
2018-08-04 17:39:32 +0530

    guessmynumber

--- M6/p3/digit_product.py
+++ M6/p3/digit_product.py
@@ -3,17 +3,17 @@ Given a  number int_input, find the product of all the digits
 example: 
 	input: 123
 	output: 6
-'''
+''' '''
 def main():
-    '''
+    ''' '''
     Read any number from the input, store it in variable int_input.
     '''
     int_input = int(input(123))
-    while N_ > 0:
-        N1_ = N%10
-        S_ = S_*N1_
-        N_ = N_//10
-        if N3_ >= 0:
-        	print(S_)
+    while N > 0:
+        N1 = N%10
+        S = S*N1
+        N = N//10
+        if N3 >= 0:
+        	print(S)
 if __name__ == "__main__":
     main()
