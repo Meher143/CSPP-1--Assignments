38b8ca1ea70fb57725b209009dccb6fe8cce61f8
Meher143 <krishmeher@msitprogram.net>
2018-08-06 23:27:16 +0530

    " -> 10 of 10 passed. pylint: 10.00/10 "

--- M7/Functions - Assignment-1/assignment1.py
+++ M7/Functions - Assignment-1/assignment1.py
@@ -1,39 +1,20 @@
-# Functions | Assignment-1 - Paying Debt off in a Year
-
-# Write a program to calculate the credit card balance after one year if a person only pays the minimum monthly payment required by the
-# credit card company each month.
-
-# The following variables contain values as described below:
-# balance - the outstanding balance on the credit card
-# annualInterestRate - annual interest rate as a decimal
-# monthlyPaymentRate - minimum monthly payment rate as a decimal
-
-# For each month, calculate statements on the monthly payment and remaining balance. At the end of 12 months, print out the remaining
-# balance. Be sure to print out no more than two decimal digits of accuracy - so print
-
-# Remaining balance: 813.41
-# instead of
-# Remaining balance: 813.4141998135
-
-# So your program only prints out one thing: the remaining balance at the end of the year in the format:
-# Remaining balance: 4784.0
-
-# A summary of the required math is found below:
-# Monthly interest rate= (Annual interest rate) / 12.0
-# Minimum monthly payment = (Minimum monthly payment rate) x (Previous balance)
-# Monthly unpaid balance = (Previous balance) - (Minimum monthly payment)
-# Updated balance each month = (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance)
-
-
-def payingDebtOffInAYear(balance, annualInterestRate, monthlyPaymentRate):
-	
-
+'''Calculatingpaying_debtoffinayear.'''
+def paying_debtoffinayear(balance_num, annual_interestrate, monthly_paymentrate):
+    '''paying_debtoffinayear function.'''
+    monthly_interestrate = (annual_interestrate)/12.0
+    minimum_monthlypayment = (monthly_paymentrate)*balance_num
+    monthly_unpaidbalance = balance_num-minimum_monthlypayment
+    updatedbalance_num = monthly_unpaidbalance+(monthly_interestrate*monthly_unpaidbalance)
+    return updatedbalance_num
 def main():
-	data = input()
-	data = data.split(' ')
-	data = list(map(float, data))
-	print(payingDebtOffInAYear(data[0],data[1],data[2]))
-
-if __name__== "__main__":
-	main()
-
+    '''Main Function.'''
+    data = input()
+    data = data.split(' ')
+    data = list(map(float, data))
+    i = 0
+    while i != 12:
+        data[0] = paying_debtoffinayear(data[0], data[1], data[2])
+        i = i+1
+    print("Remaining balance:", round(data[0], 2))
+if __name__ == "__main__":
+    main()
