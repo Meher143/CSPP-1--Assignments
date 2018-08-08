1f4e92e3bdd56cbf838b0d050687d7622be9767b
Meher143 <krishmeher@msitprogram.net>
2018-08-08 21:42:59 +0530

    " -> 9 of 9 passed. pylint: 10.00/10 "

--- M9/p1/assignment1.py
+++ M9/p1/assignment1.py
@@ -14,12 +14,16 @@ def is_word_guessed(secret_word, letters_guessed):
     returns: boolean, True if all the letters of secret_word are in letters_guessed;
       False otherwise
     '''
-    # FILL IN YOUR CODE HERE...
-    for i in secret_word:
-        if i not in letters_guessed:
-            return False
+    l_1 = len(secret_word)
+    l_2 = len(letters_guessed)
+    cnt_1 = 0
+    for i in range(l_1):
+        for j in range(l_2):
+            if secret_word[i] == letters_guessed[j]:
+                cnt_1 += 1
+    if cnt_1 >= l_1:
         return True
-
+    return False
 def main():
     '''
     Main function for the program
@@ -31,10 +35,10 @@ def main():
     else:
         data = []
         secret_word = ""
-    list1 = []
+    list_1 = []
     for j in range(1, len(data)):
-        list1.append(data[j][0])
-    print(is_word_guessed(secret_word, list1))
+        list_1.append(data[j][0])
+    print(is_word_guessed(secret_word, list_1))
 
 if __name__ == "__main__":
     main()
