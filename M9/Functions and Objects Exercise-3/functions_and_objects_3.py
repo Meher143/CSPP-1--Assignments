28106546cdec03c6e6016d653edc92c3247a10cc
Meher143 <krishmeher@msitprogram.net>
2018-08-09 18:55:33 +0530

    " -> 10 of 10 passed. pylint: 10.00/10 "

--- M9/Functions and Objects Exercise-3/functions_and_objects_3.py
+++ M9/Functions and Objects Exercise-3/functions_and_objects_3.py
@@ -1,17 +1,18 @@
 #Exercise : Function and Objects Exercise-3
 #Implement a function that converts the given testList = [1, -4, 8, -9] into [1, 16, 64, 81]
-
-
 def apply_to_each(L, f):
-    
-    
+    list_1 = []
+    l = len(L)
+    for i in range(l):
+        list_1 += [L[i]**f]
+    return list_1
 def main():
-	data = input()
+    data = input()
     data = data.split()
     list1 = []
-    for j in l:
+    square = 2
+    for j in data:
         list1.append(int(j))
-	apply_to_each(list1, square)
-
+    print(apply_to_each(list1, square))
 if __name__== "__main__":
-	main()
+    main()
