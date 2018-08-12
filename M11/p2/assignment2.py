ade76d50ac67a61c96bb6c1bd2e0038702a3b82f
Meher143 <krishmeher@msitprogram.net>
2018-08-12 15:59:16 +0530

    " -> 6 of 6 passed. pylint: -1.76/10 "

--- M11/p2/assignment2.py
+++ M11/p2/assignment2.py
@@ -20,22 +20,22 @@ def updateHand(hand, word):
     """
     meher = hand
     for i in word:
-   		if i in hand:
-   			meher[i] = meher[i] - 1
-   	return meher
+        if i in hand:
+            meher[i] = meher[i] - 1
+    return meher
     
 
 def main():
-	n=input()
-	adict={}
-	for i in range(int(n)):
-		data=input()
-		l=data.split()
-		adict[l[0]]=int(l[1])
-	data1=input()
-	print(updateHand(adict,data1))
-		
+    n=input()
+    adict={}
+    for i in range(int(n)):
+        data=input()
+        l=data.split()
+        adict[l[0]]=int(l[1])
+    data1=input()
+    print(updateHand(adict,data1))
+        
 
 
 if __name__== "__main__":
-	main()
+    main()
