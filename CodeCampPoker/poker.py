6ccb9a0822f3b2bc8e5950cccfe054a887678cfe
Meher143 <krishmeher@msitprogram.net>
2018-08-17 17:07:46 +0530

    " -> 19 of 19 passed. pylint: 8.93/10 "

--- CodeCampPoker/poker.py
+++ CodeCampPoker/poker.py
@@ -3,247 +3,39 @@
     Read about poker hands here.
     https://en.wikipedia.org/wiki/List_of_poker_hands
 '''
-
+def get_faceval(hand):
+    '''Function for getting the face values'''
+    return sorted(['--23456789TJQKA'.index(face) for face, suite in hand], reverse=True)
 def is_straight(hand):
-    '''
-        How do we find out if the given hand is a straight?
-        The hand has a list of cards represented as strings.
-        There are multiple ways of checking if the hand is a straight.
-        Do we need both the characters in the string? No.
-        The first character is good enough to determine a straight
-        Think of an algorithm: given the card face value how to check if it a straight
-        Write the code for it and return True if it is a straight else return False
-    '''
-    dict1={'A':14, 'K':13, 'Q':12, 'J':11,'T':10}
-    b=[]
-    for i in hand:
-        #print (i)
-        a=i[0]
-        #print(a)
-        if a in dict1.keys():
-            a=dict1[a]
-        #print (a)
-        a=int(a)
-        b.append(a)
-    #print(b)
-    b.sort()
-    #print (b)
-    d=max(b)
-    #print (d)
-    e=min(b)
-    #print (e)
-    f=d-e
-    #print (f)
-    g=len(b)
-    #print (g)
-    if f==4 and g==5:
-        return True
-    else:
-        return False
-
+    '''Function for finding straight'''
+    face_val = get_faceval(hand)
+    if face_val == [14, 5, 4, 3, 2]:
+        face_val = [5, 4, 3, 2, 1]
+    set_1 = set(face_val)
+    return (len(set_1) == 5) and ((max(set_1)-min(set_1)) == 4)
 def is_flush(hand):
-    '''
-        How do we find out if the given hand is a flush?
-        The hand has a list of cards represented as strings.
-        Do we need both the characters in the string? No.
-        The second character is good enough to determine a flush
-        Think of an algorithm: given the card suite how to check if it is a flush
-        Write the code for it and return True if it is a flush else return False
-    '''
-    dict1={'A':14, 'K':13, 'Q':12, 'J':11,'T':10}
-    b=[]
-    for i in hand:
-        #print (i)
-        a=i[1]
-        #print(a)
-        if a in dict1.keys():
-            a=dict1[a]
-        #print (a)
-        #a=int(a)
-        b.append(a)
-    #print(b)
-    c=set(b)
-    d=len(c)
-    #print (d)
-    if d==1:
-        return True
-    else:
-        return False
-def is_onepair(hand):
-    dict1={'A':14, 'K':13, 'Q':12, 'J':11,'T':10}
-    b=[]
-    for i in hand:
-        #print (i)
-        a=i[0]
-        #print(a)
-        if a in dict1.keys():
-            a=dict1[a]
-        #print (a)
-        a=int(a)
-        b.append(a)
-    #print(b)
-    c=0
-    for i in b:
-        if b.count(i)==2:
-            c=c+1
-    if c==2:
-        return True
-    else:
-        return False
-def is_fourkind(hand):
-    dict1={'A':14, 'K':13, 'Q':12, 'J':11,'T':10}
-    b=[]
-    for i in hand:
-        #print (i)
-        a=i[0]
-        #print(a)
-        if a in dict1.keys():
-            a=dict1[a]
-        #print (a)
-        a=int(a)
-        b.append(a)
-    #print(b)
-    x=0
-    c=0
-    for i in b:
-        if b.count(i)==4:
-            return True
-            #exit()
-    else:
-        return False
-def is_fullhouse(hand):
-    dict1={'A':14, 'K':13, 'Q':12, 'J':11,'T':10}
-    b=[]
-    for i in hand:
-        #print (i)
-        a=i[0]
-        #print(a)
-        if a in dict1.keys():
-            a=dict1[a]
-        #print (a)
-        a=int(a)
-        b.append(a)
-    #print(b)
-    c=0
-    d=0
-    for i in b:
-        if b.count(i)==3:
-            c=1
-        if b.count(i)==2:
-            d=1
-    if c==1 and d==1:
-        return True
-    else:
-        return False
-def is_threekind(hand):
-    dict1={'A':14, 'K':13, 'Q':12, 'J':11,'T':10}
-    b=[]
-    for i in hand:
-        #print (i)
-        a=i[0]
-        #print(a)
-        if a in dict1.keys():
-            a=dict1[a]
-        #print (a)
-        a=int(a)
-        b.append(a)
-    #print(b)
-    c=set(b)
-    e=len(c)
-    #print (e)
-
-    c=0
-    for i in b:
-        if b.count(i)==3:
-            c=c+1
-    if c==3 and e==3:
-        return True
-        #exit()
-    else:
-        return False
-def is_twopair(hand):
-    dict1={'A':14, 'K':13, 'Q':12, 'J':11,'T':10}
-    b=[]
-    for i in hand:
-        #print (i)
-        a=i[0]
-        #print(a)
-        if a in dict1.keys():
-            a=dict1[a]
-        #print (a)
-        a=int(a)
-        b.append(a)
-    #print(b)
-    c=set(b)
-    d=len(c)
-    #print (d)
-    c=0
-    for i in b:
-        if b.count(i)==2:
-            c += 1
-    if c == 4 and d==3:
-        return True
-    else:
-        return False
-
-
-
-    
-
-        
-        
- 
-        
-
+    '''Function for finding flush'''
+    set_1 = set([suite for face, suite in hand])
+    return len(set_1) == 1
+def kind(face_val, n_1):
+    '''Function for finding the kind'''
+    for face in face_val:
+        if face_val.count(face) == n_1:
+            return face
 def hand_rank(hand):
-    '''
-        You will code this function. The goal of the function is to
-        return a value that max can use to identify the best hand.
-        As this function is complex we will progressively develop it.
-        The first version should identify if the given hand is a straight
-        or a flush or a straight flush.
-    '''
-
-    # By now you should have seen the way a card is represented.
-    # If you haven't then go the main or poker function and print the hands
-    # Each card is coded as a 2 character string. Example Kind of Hearts is KH
-    if is_flush(hand) and is_straight(hand):
-        return 8
-    if is_fourkind(hand):
-        return 7
-    if is_fullhouse(hand):
-        return 6
-    if is_flush(hand):
-        return 5
-    if is_straight(hand):
-        return 4
-    if is_threekind(hand):
-        return 3
-    if is_twopair(hand):
-        return 2
-    if is_onepair(hand):
-        return 1
-
-    
-
-    
-    return 0
-
-    
-    
-    # First character for face value 2,3,4,5,6,7,8,9,T,J,Q,K,A
-    # Second character for the suit S (Spade), H (Heart), D (Diamond), C (Clubs)
-    # What would be the logic to determine if a hand is a straight or flush?
-    # Let's not think about the logic in the hand_rank function
-    # Instead break it down into two sub functions is_straight and is_flush
-
-    # check for straight, flush and straight flush
-    # best hand of these 3 would be a straight flush with the return value 3
-    # the second best would be a flush with the return value 2
-    # third would be a straight with the return value 1
-    # any other hand would be the fourth best with the return value 0
-    # max in poker function uses these return values to select the best hand
-    
+    '''Function for finding the rank of a hand'''
+    face_val = get_faceval(hand)
+    return ((8, face_val) if is_flush(hand) and is_straight(hand) else
+            (7, kind(face_val, 4), face_val) if kind(face_val, 4) else
+            (6, kind(face_val, 3), kind(face_val, 2)) if kind(face_val, 3)
+            and kind(face_val, 2) else
+            (5, face_val) if is_flush(hand) else
+            (4, face_val) if is_straight(hand) else
+            (3, kind(face_val, 3), face_val) if kind(face_val, 3) else
+            (2, kind(face_val, 2), kind(sorted(face_val), 2), face_val)
+            if kind(face_val, 2) and kind(sorted(face_val), 2) else
+            (1, kind(face_val, 2), face_val) if kind(face_val, 2) else
+            (0, face_val))
 
 def poker(hands):
     '''
@@ -255,14 +47,6 @@ def poker(hands):
 
         Output: Return the winning poker hand
     '''
-
-    # the line below may be new to you
-    # max function is provided by python library
-    # learn how it works, in particular the key argument, from the link
-    # https://www.programiz.com/python-programming/methods/built-in/max
-    # hand_rank is a function passed to max
-    # hand_rank takes a hand and returns its rank
-    # max uses the rank returned by hand_rank and returns the best hand
     return max(hands, key=hand_rank)
 
 if __name__ == "__main__":
