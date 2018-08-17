630839b1a2c06f294ad9185a273dd4cc242fda17
Meher143 <krishmeher@msitprogram.net>
2018-08-17 17:14:28 +0530

    " -> 0 of 19 passed. pylint: 8.93/10 "

--- CodeCampPoker/poker.py
+++ CodeCampPoker/poker.py
@@ -49,7 +49,7 @@ def poker(hands):
     '''
     return max(hands, key=hand_rank)
 
-if __name__ == "__main__":
+if __name__ == "__ main __":
     # read the number of test cases
     COUNT = int(input())
     # iterate through the test cases to set up hands list
