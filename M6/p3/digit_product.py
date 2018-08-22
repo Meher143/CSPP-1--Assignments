c4be666869ab8e202f0af38e881db871fdbed577
Meher143 <krishmeher@msitprogram.net>
2018-08-22 18:24:26 +0530

    " -> 7 of 7 passed. pylint: 10.00/10 "

--- M6/p3/digit_product.py
+++ M6/p3/digit_product.py
@@ -1,8 +1,5 @@
 '''
 Given a  number int_input, find the product of all the digits
-example: 
-    input: 123
-    output: 6
 '''
 def main():
     '''
@@ -29,4 +26,3 @@ def main():
         print(fou)
 if __name__ == "__main__":
     main()
-    
