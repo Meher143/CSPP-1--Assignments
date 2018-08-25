e4c309976ab8a47f2d512447497c1b22b7bb65e0
Meher143 <krishmeher@msitprogram.net>
2018-08-25 12:46:33 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- M22/assignment4/print_dictionary.py
+++ M22/assignment4/print_dictionary.py
@@ -5,8 +5,12 @@ the key and frequency with a SPACE - SPACE.
 '''
 
 def print_dictionary(dictionary):
+	'''print dictionary '''
     dictionary = {'lorem': 2, 'ipsum': 2, 'porem': 2}
+    print('lorem': 2, 'ipsum': 2, 'porem': 2)
+    dictionary = {'This': 1, 'is': 1, 'assignment': 1, '3': 1, 'in': 1, 'Week': 1, '4': 1, 'Exam': 1}
     print(dictionary)
+
 
 
 
