60b5de1865af5e8ec2c3f0b78a725e771c6701a6
Meher143 <krishmeher@msitprogram.net>
2018-08-25 12:47:16 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- M22/assignment4/print_dictionary.py
+++ M22/assignment4/print_dictionary.py
@@ -8,8 +8,7 @@ def print_dictionary(dictionary):
 	'''print dictionary '''
     dictionary = {'lorem': 2, 'ipsum': 2, 'porem': 2}
     print('lorem': 2, 'ipsum': 2, 'porem': 2)
-    dictionary = {'This': 1, 'is': 1, 'assignment': 1, '3': 1, 'in': 1, 'Week': 1, '4': 1, 'Exam': 1}
-    print(dictionary)
+    
 
 
 
