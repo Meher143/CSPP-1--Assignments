2ee76321d528e8bcd64641a72334e5b464119306
Meher143 <krishmeher@msitprogram.net>
2018-08-25 10:38:53 +0530

    " -> 0 of 2 passed. pylint: 0/0 "

---
+++ M22/assignment1/read_input.py
@@ -0,0 +1,16 @@
+'''
+Write a python program to read multiple lines of text input and store the input into a string.
+'''
+
+def main():
+	string_1 = ''
+    lines = int(input())
+    for i in range(lines):
+        i += 1
+        string_1 += input()
+        string_1 += '\n'
+        print(string_1)
+    
+
+if __name__ == '__main__':
+    main()
